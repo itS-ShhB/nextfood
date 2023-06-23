30248fc3644431fd428dba5055c4f701162eda62
itS-ShhB <its.shhwb@gmail.com>
2023-06-24 01:43:41 +0430

    Added Data For Search

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,25 +1,51 @@
-import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 function CategoriesPage() {
+  const router = useRouter();
+  const [query, setQuery] = useState({ difficulty: "", time: "" });
+  const changeHandler = (e) => {
+    setQuery({ ...query, [e.target.name]: e.target.value });
+  };
+  const searchHandler = () => {
+    console.log(query);
+    router.push({
+      pathname: "/categories",
+      query,
+    });
+  };
   return (
     <div className="max-w-4xl m-auto mt-24">
       <h2 className="border-b-4 ml-5 border-[#53c60b w-fit mb-12 text-3xl]">
         Categories
       </h2>
-      <div className='ml-5'>
+      <div className="ml-5">
         <div>
-          <select className="border-none focus:border-none cursor-pointer hover:text-white hover:bg-[#48ac0a] ease-in-out transition delay-75 w-36 h-10 rounded-xl mr-2 p-2 text-[#48ac0a] shadow-sm shadow-[#48ac0a]">
-            <option>Difficulty</option>
-            <option>Easy</option>
-            <option>Medium</option>
-            <option>Hard</option>
+          <select
+            onChange={changeHandler}
+            value={query.difficulty}
+            name="difficulty"
+            className="border-none focus:border-none cursor-pointer hover:text-white hover:bg-[#48ac0a] ease-in-out transition delay-75 w-36 h-10 rounded-xl mr-2 p-2 text-[#48ac0a] shadow-sm shadow-[#48ac0a]"
+          >
+            <option value="">Difficulty</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
           </select>
-          <select className="border-none focus:border-none cursor-pointer hover:text-white hover:bg-[#48ac0a] ease-in-out transition delay-75 w-52 h-10 rounded-xl mr-2 p-2 text-[#48ac0a] shadow-sm shadow-[#48ac0a]">
-            <option>Cooking Time</option>
-            <option>More Than 30 Minutes</option>
-            <option>Less Than 30 Minutes</option>
+          <select
+            onChange={changeHandler}
+            value={query.time}
+            name="time"
+            className="border-none focus:border-none cursor-pointer hover:text-white hover:bg-[#48ac0a] ease-in-out transition delay-75 w-52 h-10 rounded-xl mr-2 p-2 text-[#48ac0a] shadow-sm shadow-[#48ac0a]"
+          >
+            <option value="">Cooking Time</option>
+            <option value="More">More Than 30 Minutes</option>
+            <option value="Less">Less Than 30 Minutes</option>
           </select>
-          <button className="bg-[#53c60b] text-white h-9 py-0 px-5 rounded-lg cursor-pointer">
+          <button
+            onClick={searchHandler}
+            className="bg-[#53c60b] text-white h-9 py-0 px-5 rounded-lg cursor-pointer"
+          >
             Search
           </button>
         </div>
--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,7 +1,40 @@
 import CategoriesPage from "../../components/templates/CategoriesPage";
 
-function Categories() {
-  return <CategoriesPage />;
+function Categories({ data }) {
+  console.log(data);
+  return <CategoriesPage data={data} />;
 }
 
 export default Categories;
+
+export async function getServerSideProps(context) {
+  const {
+    query: { difficulty, time },
+  } = context;
+  const res = await fetch("http://localhost:3001/data");
+  const data = await res.json();
+
+  const filteredData = data.filter((item) => {
+    const difficultyResult = item.details.filter(
+      (detail) => detail.Difficulty && detail.Difficulty === difficulty
+    );
+
+    const timeResult = item.details.filter((detail) => {
+      const cookingTime = detail["Cooking Time"] || "";
+      const [timeDetail] = cookingTime.split(" ");
+      if (time === "Less" && timeDetail && +timeDetail <= 30) {
+        return detail;
+      } else if (time === "More" && +timeDetail > 30) {
+        return detail;
+      }
+    });
+    if (time && difficulty && timeResult.length && difficultyResult.length) {
+      return item;
+    } else if (!time && difficulty && difficultyResult.length) {
+      return item;
+    } else if (time && !difficulty && timeResult.length) {
+      returnitem;
+    }
+  });
+  return { props: { data: filteredData } };
+}
